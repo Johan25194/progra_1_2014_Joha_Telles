d07e1982467fbe06077bc0e5df376a7abbb6d9bc
Johan25194 <johan25194@gmail.com>
2014-05-16 10:20:02 -0600

    class_4

---
+++ Practice/practice_2/appPractica_2/src/apppractica_2/AppPractica_2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package apppractica_2;
+
+/**
+ *
+ * @author JohanAdrian
+ */
+public class AppPractica_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
