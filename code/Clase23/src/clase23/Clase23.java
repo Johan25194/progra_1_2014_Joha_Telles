02e84272c0956e6e6582cc18a3e722f9f6991ef0
Johan25194 <johan25194@gmail.com>
2014-08-11 18:33:39 -0600

    ss

---
+++ code/Clase23/src/clase23/Clase23.java
@@ -0,0 +1,25 @@
+/*
+*Name:CLss1.
+ *Information:This is the first class
+*Date:08/08/2014
+*Copyright
+ */
+
+package clase23;
+
+/**
+ * 
+ * @author Johan
+ */
+public class Clase23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmMain oMain=new frmMain();
+        oMain.setVisible(true);
+    }
+    
+}
